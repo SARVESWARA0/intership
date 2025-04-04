e0270a3bcbd4cb31ae564a86e4428bacff6f3d3c
harikrishnan-crayond <harikrishnan@crayond.com>
2025-04-04 17:51:41 +0530

    useLayoutEffect import removed

--- src/app/task/[encoded]/page.js
+++ src/app/task/[encoded]/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useLayoutEffect, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import { Buffer } from 'buffer';
 
